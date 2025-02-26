8aea7dbfb1ca35f8b6fef63ec65e6b79ad58a707
Leih Balbin <leihaldrin.balbin@college.cksc.edu.ph>
2025-02-26 09:18:30 +0800

    Continued with RegisterScreen.kt and added simple validation

--- app/src/main/java/ph/edu/cksc/college/appdev/appdev2025/screens/RegisterScreen.kt
+++ app/src/main/java/ph/edu/cksc/college/appdev/appdev2025/screens/RegisterScreen.kt
@@ -4,11 +4,13 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -24,9 +26,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import ph.edu.cksc.college.appdev.appdev2025.ui.theme.AppDev2025Theme
@@ -73,24 +77,30 @@ fun RegisterScreen(navController: NavHostController) {
 @Composable
 fun RegisterScrollContent(innerPadding: PaddingValues) {
     var data by mutableStateOf(RegistrationData())
+    var username by rememberSaveable { mutableStateOf("") }
+    var email by rememberSaveable { mutableStateOf("") }
     var password by rememberSaveable { mutableStateOf("") }
+    var retypePassword by rememberSaveable { mutableStateOf("") }
+    var error by rememberSaveable { mutableStateOf("") }
+
     Box(
         modifier = Modifier.padding(innerPadding)
+            .padding(4.dp)
     ) {
         Column() {
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
-                value = data.username,
+                value = username,
                 onValueChange = {
-                    data = data.copy(username = it)
+                    username = it
                 },
                 label = { Text("Username") }
             )
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
-                value = data.email,
+                value = email,
                 onValueChange = {
-                    data = data.copy(email = it)
+                    email = it
                 },
                 label = { Text("Email") }
             )
@@ -106,15 +116,29 @@ fun RegisterScrollContent(innerPadding: PaddingValues) {
             )
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
-                value = data.retypePassword,
+                value = retypePassword,
                 onValueChange = {
-                    data = data.copy(retypePassword = it)
+                    retypePassword = it
                 },
                 label = { Text("Retype Password") },
                 visualTransformation = PasswordVisualTransformation(),
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
             )
-
+            Row(modifier = Modifier.padding(top = 8.dp)) {
+                Button(onClick = {
+                    error = ""
+                    if (password != retypePassword) {
+                        error = "Password didn't match"
+                    }
+                }) {
+                    Text("Register")
+                }
+                Button(onClick = { /* handle click */ },
+                    modifier = Modifier.padding(start = 192.dp)) {
+                    Text("Cancel")
+                }
+            }
+            Text(color = Color.Red, text = error)
         }
     }
 }
