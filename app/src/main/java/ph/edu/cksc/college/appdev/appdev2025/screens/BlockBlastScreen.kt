ed04ff440fb2befafe10592e729440933350109a
Art Rivera <artvincent.rivera@college.cksc.edu.ph>
2025-05-09 13:08:09 +0800

    Made a BlockBlastScreen.kt, still work in progress.

---
+++ app/src/main/java/ph/edu/cksc/college/appdev/appdev2025/screens/BlockBlastScreen.kt
@@ -0,0 +1,26 @@
+package ph.edu.cksc.college.appdev.appdev2025.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import kotlin.math.roundToInt
+import kotlin.random.Random
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.platform.LocalDensity
+
+const val BLOCK_BLAST_SCREEN = "block_blast_screen"
+
+
